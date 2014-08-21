6f7e487da07165de92e2f19be75536c85d9694e2
PalakPC <no.1palak@gmail.com>
2014-08-21 18:55:47 +0530

    Nodejs config.js removed

--- panel/panel.java
+++ panel/panel.java
@@ -17,22 +17,22 @@ public class panel extends JPanel {
 	class Panel1 extends JPanel implements ActionListener {
 		private JButton jcomp1;
 	
-		public Panel1 () {
-			jcomp1 = new JButton ("Next");
-			setPreferredSize (new Dimension (400, 200));
-			setLayout (null);
-			add (jcomp1);
-			jcomp1.setBounds (150, 75, 100, 30);
-			jcomp1.addActionListener (this);
+		public Panel1() {
+			jcomp1 = new JButton("Next");
+			setPreferredSize(new Dimension(400, 200));
+			setLayout(null);
+			add(jcomp1);
+			jcomp1.setBounds(150, 75, 100, 30);
+			jcomp1.addActionListener(this);
 		}
 	
-		public void actionPerformed (ActionEvent e) {
-			frame2 = new JFrame ("Window 2");
-			frame2.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-			frame2.getContentPane ().add (new Panel2 ());
-			frame2.pack ();
-			frame2.setVisible (true);
-			frame1.setVisible (false);
+		public void actionPerformed(ActionEvent e) {
+			frame2 = new JFrame("Window 2");
+			frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame2.getContentPane().add(new Panel2());
+			frame2.pack();
+			frame2.setVisible(true);
+			frame1.setVisible(false);
 		}
 	}
 
@@ -40,73 +40,73 @@ public class panel extends JPanel {
 		private JButton jcomp1;
 		private JButton jcomp2;
 
-		public Panel2 () {
-			jcomp1 = new JButton ("Previous");
-			jcomp2 = new JButton ("Next");
-			setPreferredSize (new Dimension (400, 200));
-			setLayout (null);
-			add (jcomp1);
-			add (jcomp2);
-			jcomp1.setBounds (75, 75, 100, 30);
-			jcomp2.setBounds (225, 75, 100, 30);
-			jcomp1.addActionListener (this);
-			jcomp2.addActionListener (this);
+		public Panel2() {
+			jcomp1 = new JButton("Previous");
+			jcomp2 = new JButton("Next");
+			setPreferredSize(new Dimension(400, 200));
+			setLayout(null);
+			add(jcomp1);
+			add(jcomp2);
+			jcomp1.setBounds(75, 75, 100, 30);
+			jcomp2.setBounds(225, 75, 100, 30);
+			jcomp1.addActionListener(this);
+			jcomp2.addActionListener(this);
 		}
 
-		public void actionPerformed (ActionEvent e) {
-			if (e.getSource () == jcomp1){
-				frame1 = new JFrame ("Window 1");
-				frame1.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-				frame1.getContentPane ().add (new Panel1 ());
-				frame1.pack ();
-				frame1.setVisible (true);
+		public void actionPerformed(ActionEvent e) {
+			if(e.getSource() == jcomp1) {
+				frame1 = new JFrame("Window 1");
+				frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame1.getContentPane().add(new Panel1());
+				frame1.pack();
+				frame1.setVisible(true);
 			}
 		
 			else {
-				frame3 = new JFrame ("Window 3");
-				frame3.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-				frame3.getContentPane ().add (new Panel3 ());
-				frame3.pack ();
-				frame3.setVisible (true);
+				frame3 = new JFrame("Window 3");
+				frame3.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame3.getContentPane().add(new Panel3());
+				frame3.pack();
+				frame3.setVisible(true);
 			}
 
-			frame2.setVisible (false);
+			frame2.setVisible(false);
 		}
 	}
 
 	class Panel3 extends JPanel implements ActionListener{
 		private JButton jcomp1;
 	
-		public Panel3 () {
-			jcomp1 = new JButton ("Previous");
-			setPreferredSize (new Dimension (400, 200));
-			setLayout (null);
-			add (jcomp1);
-			jcomp1.setBounds (150, 75, 100, 30);
-			jcomp1.addActionListener (this);
+		public Panel3() {
+			jcomp1 = new JButton("Previous");
+			setPreferredSize(new Dimension(400, 200));
+			setLayout(null);
+			add(jcomp1);
+			jcomp1.setBounds(150, 75, 100, 30);
+			jcomp1.addActionListener(this);
 		}
 		
-		public void actionPerformed (ActionEvent e) {
-			frame2 = new JFrame ("Window 2");
-			frame2.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
-			frame2.getContentPane ().add (new Panel2());
-			frame2.pack ();
-			frame2.setVisible (true);
-			frame3.setVisible (false);
+		public void actionPerformed(ActionEvent e) {
+			frame2 = new JFrame("Window 2");
+			frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame2.getContentPane().add(new Panel2());
+			frame2.pack();
+			frame2.setVisible(true);
+			frame3.setVisible(false);
 		}
 	}
 
-	Panel1 makepanel1 () {
-		return new Panel1 ();
+	Panel1 makepanel1() {
+		return new Panel1();
 	}
 	
-	public static void main (String[] args) {
-		frame1 = new JFrame ("Window 1");
+	public static void main(String[] args) {
+		frame1 = new JFrame("Window 1");
 		panel x;
-		x = new panel ();
-		frame1.setDefaultCloseOperation (JFrame.HIDE_ON_CLOSE);
-		frame1.getContentPane ().add (x.makepanel1 ());
-		frame1.pack ();
-		frame1.setVisible (true);
+		x = new panel();
+		frame1.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+		frame1.getContentPane().add(x.makepanel1());
+		frame1.pack();
+		frame1.setVisible(true);
 	}
 }
--- sort/sort.java
+++ sort/sort.java
@@ -7,70 +7,70 @@ public class sort {
 	static int num;
 
 	public static class qsort {
-		public void sort (float[] values, int x) {
-			if (values == null || values.length == 0)
+		public void sort(float[] values, int x) {
+			if(values == null || values.length == 0)
 				return;
 
 			numbers = values;
 			num = x;
-			quicksort (0, num - 1);
+			quicksort(0, num - 1);
 		}
 
-		private void quicksort (int low, int high) {
+		private void quicksort(int low, int high) {
 			int i = low, j = high;
 			float pivot = numbers[low + (high - low) / 2];
 
-			while (i <= j) {
-				while (numbers[i] < pivot)
+			while(i <= j) {
+				while(numbers[i] < pivot)
 					++i;
 				
-				while (numbers[j] > pivot)
+				while(numbers[j] > pivot)
 					--j;
 
-				if (i <= j) {
-					exchange (i, j);
+				if(i <= j) {
+					exchange(i, j);
 					++i; 
 					--j;
 				}
 			}
 
-			if (low < j)
-				quicksort (low, j);
+			if(low < j)
+				quicksort(low, j);
 
-			if (i < high)
-				quicksort (i, high);
+			if(i < high)
+				quicksort(i, high);
 		}
 
-		private void exchange (int i, int j) {
+		private void exchange(int i, int j) {
 			float temp = numbers[i];
 			numbers[i] = numbers[j];
 			numbers[j] = temp;
 		}
 	}
 
-	public static void main (String[] args) {
+	public static void main(String[] args) {
 		arr = new float[50];
 	
-		while (n < args.length) {
+		while(n < args.length) {
 			try {
-				float a = Float.parseFloat (args[n]);	/*Parsing command-line arguments to float.*/
+				float a = Float.parseFloat(args[n]);	/*Parsing command-line arguments to float.*/
 				arr[n] = a;
 			}
 			
-			catch (NumberFormatException e) {
-				System.out.println ("Your argument is a non-float");
-				System.exit (1);
+			catch(NumberFormatException e) {
+				System.out.println("Your argument is a non-float");
+				System.exit(1);
 			}
 			
 			++n;
 		}
 		
-		qsort q = new qsort ();
-		q.sort (arr, n);
+		qsort q = new qsort();
+		q.sort(arr, n);
 
 		n = 0;
-		while (n < args.length) {
-			System.out.println (numbers[n]);
+		while(n < args.length) {
+			System.out.println(numbers[n]);
 			++n;
 		}
 	}	
