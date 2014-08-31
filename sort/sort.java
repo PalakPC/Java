f801b25ed2c436c5bbf6e62189d54d2659d634ea
PalakPC <no.1palak@gmail.com>
2014-08-31 20:34:35 +0530

    merge conflict resolved

--- sort/sort.java
+++ sort/sort.java
@@ -1,77 +1,61 @@
 /*Java program to sort the given command-line float arguments in ascending order using quicksort algorithm.*/
 
 public class sort { 
-	static int n = 0;
-	static float[] arr;
-	static float[] numbers;
-	static int num;
+   static float[] numbers;
+   static int num = 0;
 
-	public static class qsort {
-		public void sort(float[] values, int x) {
-			if(values == null || values.length == 0)
-				return;
+   public static class qsort {
+      public void sort() {
+         if(numbers == null || num == 0)
+            return;
+         quicksort(0, num - 1);
+      }
 
-			numbers = values;
-			num = x;
-			quicksort(0, num - 1);
-		}
+      private void quicksort(int low, int high) {
+         int i = low, j = high;
+         float pivot = numbers[low + (high - low) / 2];
+         while(i <= j) {
+            while(numbers[i] < pivot)
+               ++i;		
+            while(numbers[j] > pivot)
+               --j;
+            if(i <= j) {
+               exchange(i, j);
+               ++i; 
+               --j;
+            }
+         }
+         if(low < j)
+            quicksort(low, j);
+         if(i < high)
+            quicksort(i, high);
+      }
 
-		private void quicksort(int low, int high) {
-			int i = low, j = high;
-			float pivot = numbers[low + (high - low) / 2];
+      private void exchange(int i, int j) {
+         float temp = numbers[i];
+         numbers[i] = numbers[j];
+         numbers[j] = temp;
+      }
+   }
 
-			while(i <= j) {
-				while(numbers[i] < pivot)
-					++i;
-				
-				while(numbers[j] > pivot)
-					--j;
-
-				if(i <= j) {
-					exchange(i, j);
-					++i; 
-					--j;
-				}
-			}
-
-			if(low < j)
-				quicksort(low, j);
-
-			if(i < high)
-				quicksort(i, high);
-		}
-
-		private void exchange(int i, int j) {
-			float temp = numbers[i];
-			numbers[i] = numbers[j];
-			numbers[j] = temp;
-		}
-	}
-
-	public static void main(String[] args) {
-		arr = new float[50];
-	
-		while(n < args.length) {
-			try {
-				float a = Float.parseFloat(args[n]);	/*Parsing command-line arguments to float.*/
-				arr[n] = a;
-			}
-			
-			catch(NumberFormatException e) {
-				System.out.println("Your argument is a non-float");
-				System.exit(1);
-			}
-			
-			++n;
-		}
-		
-		qsort q = new qsort();
-		q.sort(arr, n);
-
-		n = 0;
-		while(n < args.length) {
-			System.out.println(numbers[n]);
-			++n;
-		}
-	}	
+   public static void main(String[] args) {
+      numbers = new float[50];	
+      while(num < args.length) {
+         try {
+            numbers[num] =  Float.parseFloat(args[num]);    /*Parsing command-line arguments to float.*/
+         }			
+         catch(NumberFormatException e) {
+            System.out.println("Your argument is a non-float");
+            System.exit(1);
+         }	
+         ++num;
+      }		
+      qsort q = new qsort();
+      q.sort();
+      num = 0;
+      while(num < args.length) {
+         System.out.println(numbers[num]);
+         ++num;
+      }
+   }	
 }	
